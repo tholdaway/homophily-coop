de4ab1c96379232fbb2d921336d798368ac6226f
Taylor Holdaway <taylorholdaway@pop-os.localdomain>
2020-12-21 16:43:03 -0500

    ugh

--- experiment.js
+++ experiment.js
@@ -367,8 +367,7 @@ var coop_comparison_block = {
     } else {
       stim = "Your counterpart chose to cooperate more than 75% of all players."
     }
-    stim = "In total your score was " + score_self ".<br>Your counterpart's score was " + score_other + ".<br>" +
-      stim;
+    stim = "In total your score was " + score_self + ".<br>Your counterpart's score was " + score_other + ".<br>" + stim;
     return stim
   },
   choices: jsPsych.NO_KEYS,
